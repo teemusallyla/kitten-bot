d0ad2e9701af08b922eabfc55ee44ec31799f804
Teemu Sällylä <teemu.sallyla@gmail.com>
2018-12-21 13:38:43 +0200

    Added command for refreshing and colour to embeds

--- KittenBot.py
+++ KittenBot.py
@@ -1,6 +1,5 @@
 import discord
-from RedditApi import get_random_cat
-from RedditApi import get_random_kitten
+from RedditApi import *
 
 with open("token.txt") as f:
     token = f.read()
@@ -10,12 +9,20 @@ class KittenClient(discord.Client):
     async def on_message(self, message):
         if message.content == "k!kittens":
             url = await get_random_kitten()
-            emb = discord.Embed().set_image(url=url)
+            emb = discord.Embed(
+                color=12127009
+            ).set_image(url=url)
             await self.send_message(message.channel, content="Kittens incoming!", embed=emb)
         elif message.content == "k!cats":
             url = await get_random_cat()
-            emb = discord.Embed().set_image(url=url)
+            emb = discord.Embed(
+                color=12127009
+            ).set_image(url=url)
             await self.send_message(message.channel, content="Cats incoming!", embed=emb)
+        elif message.content == "k!refresh":
+            await initialize_kittens()
+            await initialize_cats()
+            await self.send_message(message.channel, "Done!")
         elif message.content == "k!help":
             await self.send_message(message.channel, "I have only two commands: k!kittens and k!cats")
 
