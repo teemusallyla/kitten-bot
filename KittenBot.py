8a4a59b61ab2fb5527bdc44a599eb7db43785934
Teemu Sällylä <teemu.sallyla@gmail.com>
2018-12-22 01:04:53 +0200

    Added more ways to call the bot

--- KittenBot.py
+++ KittenBot.py
@@ -4,15 +4,28 @@ from RedditApi import get_random_cat, get_random_kitten, refresh_lists
 with open("token.txt") as f:
     token = f.read()
 
+kitten_commands = [
+    "k!kittens",
+    "k!kitten",
+    "k!kitty",
+    "k!kitties"
+]
+cat_commands = [
+    "k!cats",
+    "k!cat",
+    "k!catty",
+    "k!catties"
+]
+
 class KittenClient(discord.Client):
     async def on_message(self, message):
-        if message.content == "k!kittens":
+        if message.content in kitten_commands:
             url = await get_random_kitten()
             emb = discord.Embed(
                 color=12127009
             ).set_image(url=url)
             await self.send_message(message.channel, content="Kittens incoming!", embed=emb)
-        elif message.content == "k!cats":
+        elif message.content in cat_commands:
             url = await get_random_cat()
             emb = discord.Embed(
                 color=12127009
