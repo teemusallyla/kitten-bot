b5cbf35d15a571811fc7e98858d22ab02db413c7
Teemu Sällylä <teemu.sallyla@gmail.com>
2018-12-24 20:58:22 +0200

    Added logging, typing message

--- KittenBot.py
+++ KittenBot.py
@@ -20,22 +20,44 @@ cat_commands = [
 class KittenClient(discord.Client):
     async def on_message(self, message):
         if message.content in kitten_commands:
+            await self.send_typing(message.channel)
             url = await get_random_kitten()
             emb = discord.Embed(
                 color=12127009
             ).set_image(url=url)
-            await self.send_message(message.channel, content="Kittens incoming!", embed=emb)
+            await self.send_message(
+                message.channel,
+                content="Kittens incoming!",
+                embed=emb)
+            with open("log.log", "a+") as f:
+                f.write("k!kittens by {} in {}".format(
+                    message.author.name,
+                    message.server))
+
         elif message.content in cat_commands:
+            await self.send_typing(message.channel)
             url = await get_random_cat()
             emb = discord.Embed(
                 color=12127009
             ).set_image(url=url)
-            await self.send_message(message.channel, content="Cats incoming!", embed=emb)
+            await self.send_message(
+                message.channel,
+                content="Cats incoming!",
+                embed=emb)
+            with open("log.log", "a+") as f:
+                f.write("k!cats by {} in {}".format(
+                    message.author.name,
+                    message.server))
+
         elif message.content == "k!refresh":
+            await self.send_typing(message.channel)
             await refresh_lists()
             await self.send_message(message.channel, "Done!")
+
         elif message.content == "k!help":
-            await self.send_message(message.channel, "I have only two commands: k!kittens and k!cats")
+            await self.send_message(
+                message.channel,
+                "I have only two commands: k!kittens and k!cats")
 
     async def on_ready(self):
         game = discord.Game(name="k!kittens")
